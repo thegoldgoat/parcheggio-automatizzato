a0cef1bd5a53331128f4642c01d1740c8a76bcf0
Fabio Fontana <fabifont.scuola@gmail.com>
2018-11-09 20:31:46 +0100

    Aggiunta funzione di uscita , ora il client JS funziona perfettamente con il server

--- src/client/client.js
+++ src/client/client.js
@@ -1,8 +1,8 @@
 function AggiungiMacchina()
 {
     const API_URL = "http://localhost:8080/";
-    var form = $('form').serializeArray();
-    var arr = {id_parcheggio:form[0].value, targa:form[1].value};
+    var form = $('form[name="formArrivo"]').serializeArray();
+    var arr = {id_parcheggio: parseInt(form[0].value), targa:form[1].value};
     $.ajax(
     {
         url: (API_URL + "arrivoMacchina"),
@@ -15,4 +15,28 @@ function AggiungiMacchina()
             alert(msg);
         }
     });
+    $('form[name="formArrivo"]').trigger("reset");
 };
+
+function UscitaMacchina()
+{
+    const API_URL = "http://localhost:8080/";
+    var form = $('form[name="formUscita"]').serializeArray();
+    var arr = {id_parcheggio: parseInt(form[0].value), targa:form[1].value};
+    $.ajax(
+    {
+        url: (API_URL + "uscitaMacchina"),
+        type: "POST",
+        data: JSON.stringify(arr),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        async: false,
+        success: function(data, statusText, xhr) {
+            if(xhr.status == 200)
+                alert(JSON.stringify(data));
+            else
+                alert("Targa rifiutata");
+        }
+    });
+    $('form[name="formUscita"]').trigger("reset");
+}
