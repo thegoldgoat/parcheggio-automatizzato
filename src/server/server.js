485756f7506cae494ec0fd2e25f01eaa6229f3e8
matteo ballabio <ballabio.matteo.00@gmail.com>
2018-11-09 23:14:53 +0100

    Modificati permessi CORS

--- src/server/server.js
+++ src/server/server.js
@@ -27,7 +27,12 @@ var macchineParcheggiateSchema = mongoose.Schema({
 var macchineParcheggiateModel = mongoose.model('macchineParcheggiate', macchineParcheggiateSchema);
 
 var app = express();
-app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    express.json();
+});
 
 app.post('/arrivoMacchina', function (req, res) {
     console.log('arrivoMacchina: Targa ricevuta -> ', req.body);
