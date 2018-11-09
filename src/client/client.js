324f16c78bf03ab6a6e15fb632718b184c0b1057
Fabio Fontana <fabifont.scuola@gmail.com>
2018-11-09 19:59:24 +0100

    Aggiunta la parte di html che implementa il client JS

--- src/client/client.js
+++ src/client/client.js
@@ -1,14 +1,18 @@
-const API_URL = "http://localhost:8080/";
-var arr = {id_parcheggio:1, targa:"DE123FG"};
-$.ajax(
-   {
+function AggiungiMacchina()
+{
+    const API_URL = "http://localhost:8080/";
+    var form = $('form').serializeArray();
+    var arr = {id_parcheggio:form[0].value, targa:form[1].value};
+    $.ajax(
+    {
         url: (API_URL + "arrivoMacchina"),
         type: "POST",
-        data: arr,
+        data: JSON.stringify(arr),
+        contentType: 'application/json; charset=utf-8',
         dataType: 'json',
         async: false,
         success: function(msg) {
             alert(msg);
         }
-    }
-);
+    });
+};
